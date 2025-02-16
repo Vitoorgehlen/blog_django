5988414041327e79f2dd00635cada687811bbfbc
Vitor Gehlen <vitoorgehlen@gmail.com>
2025-02-16 16:18:17 -0300

    Atualização para Railway

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -3,8 +3,8 @@
 # O shell irá encerrar a execução do script quando um comando falhar
 set -e
 
-wait_psql.sh
+./wait_psql.sh
 
-collectstatic.sh
-migrate.sh
-runserver.sh
+./collectstatic.sh
+./migrate.sh
+./runserver.sh
